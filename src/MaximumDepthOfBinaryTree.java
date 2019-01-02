92ba6ba904629d96de95d38260c297ed891ba4c9
name <kelvin94@live.cn>
2019-01-02 00:17:48 -0800

    init commit

---
+++ src/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,25 @@
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        int counter = dfs(root, 0);
+        return counter;
+    }
+    public int dfs(TreeNode node, int counter) {
+        if(node == null) { return counter; }
+        else {
+            counter+=1;
+            int counter1 = dfs(node.left, counter);
+            int counter2 = dfs(node.right, counter);
+            if(counter1 > counter2){ return counter1; }
+            else {return counter2;}
+        }
+    }
+    //############# LeetCode答案
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
--- src/ReverseString.java
+++ src/ReverseString.java
@@ -9,4 +9,18 @@ public class ReverseString {
         }
         return result;
     }
+    // ################ Leetcode上面的答案
+//    public String reverseString(String s) {
+//        char[] word = s.toCharArray();
+//        int i = 0;
+//        int j = s.length() - 1;
+//        while (i < j) {
+//            char temp = word[i];
+//            word[i] = word[j];
+//            word[j] = temp;
+//            i++;
+//            j--;
+//        }
+//        return new String(word);
+//    }
 }
